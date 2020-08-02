4011eb6c36d18a3e763db3dc1dcc84ef7446e281
schand143 <shaikhchand724@gmail.com>
2020-08-02 19:47:41 +0530

    Updates

---
+++ precache-manifest.30982c913f096d49a229d2294d10a363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732a4ab33e0537fb595f59c41323ab06",
+    "url": "/pics-browser/index.html"
+  },
+  {
+    "revision": "8572d553d13afa81ceb5",
+    "url": "/pics-browser/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c8e09c81690375b8c952",
+    "url": "/pics-browser/static/js/2.7f3c411b.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/pics-browser/static/js/2.7f3c411b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8572d553d13afa81ceb5",
+    "url": "/pics-browser/static/js/main.a4fef5db.chunk.js"
+  },
+  {
+    "revision": "15edd052cdb3d6f90fd3",
+    "url": "/pics-browser/static/js/runtime-main.7ed9223e.js"
+  }
+]);
